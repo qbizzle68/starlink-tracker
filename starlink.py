e3e840dbe93456a419aec73cbe063b11952809d0
qbarnes <quinton.barnes68@gmail.com>
2023-09-21 18:43:15 -0500

    capable of generating split frames

--- starlink.py
+++ starlink.py
@@ -1,6 +1,7 @@
 import sys
 import tomllib
 from math import degrees
+from pprint import pp
 
 import pandas as pd
 from sattrack.orbit import Satellite
@@ -51,11 +52,23 @@ def computeApsides(sma: float, ecc: float) -> (int, int):
     rp = sma * (1 - ecc)
     ra = sma * (1 + ecc)
 
-    return (rp - EARTH_EQUITORIAL_RADIUS, ra - EARTH_EQUITORIAL_RADIUS)
+    return rp - EARTH_EQUITORIAL_RADIUS, ra - EARTH_EQUITORIAL_RADIUS
 
 
-def adjustLatitudeArguments(rawArgs):
-    # rawArgs = [degrees(el.aop + el.trueAnomaly) % 360.0 for el in elements]
+SCALE = 1.0
+
+
+def findOutliersIQR(df: pd.Series) -> pd.Series:
+    q1 = df.quantile(0.25)
+    q3 = df.quantile(0.75)
+    IQR = q3 - q1
+
+    # We shouldn't worry about the small outliers, as those satellites are in the same group.
+    return df[df > (q3 + SCALE * IQR)]
+
+
+def adjustLatitudeArguments(df):
+    rawArgs = df['latArg'].tolist()
     diffs = [rawArgs[i] - rawArgs[i - 1] for i in range(len(rawArgs))]
     maxValue = max(diffs)
     maxIndex = diffs.index(maxValue)
@@ -79,37 +92,114 @@ def adjustLatitudeArguments(rawArgs):
     return rtn
 
 
-def adjustPhase(rawArgs):
-    diffs = [rawArgs[i] - rawArgs[i - 1] for i in range(len(rawArgs))]
-    diffs[0] += 360
+def adjustPhase(df: pd.DataFrame) -> pd.DataFrame:
+    # Index needs to be in ascending order for this to work.
+    maxIndex = df['gap'].idxmax()
+    # If maxIndex is zero then the rows are already in order.
+    if maxIndex != 0:
+        arr = df['phase'].tolist()
+        # All rows from maxIndex to the end should precede the first rows, adjust by subtracting 360.
+        for i in range(maxIndex, len(arr)):
+            arr[i] -= 360
+
+        df = df.assign(phase=arr)
+
+    maxValue = df['phase'].max()
+    df['phase'] = df['phase'].apply(lambda x: x - maxValue)
+    df.sort_values('phase', ascending=False, inplace=True)
+
+    return df.reset_index(drop=True)
 
 
-
-def buildDataFrame(tleList, jd, adjust=True) -> pd.DataFrame:
+def generateDataFrame(tleList, jd, adjust=True) -> pd.DataFrame:
     elements = [Satellite(tle).getElements(jd) for tle in tleList]
     apsides = [computeApsides(el.sma, el.ecc) for el in elements]
-    latArgs = [degrees(el.aop + el.trueAnomaly) % 360.0 for el in elements]
+    phases = [degrees(el.aop + el.trueAnomaly) % 360.0 for el in elements]
     data = {
         'name': (tle.name for tle in tleList),
         'inclination': (degrees(el.inc) for el in elements),
         'raan': (degrees(el.raan) for el in elements),
-        'latArg': latArgs,
+        'phase': phases,
         'perigee': [round(rp, 1) for rp, _ in apsides],
         'apogee': [round(ra, 1) for _, ra in apsides],
     }
 
-    df = pd.DataFrame(data).sort_values('latArg')
+    # Sort rows by latArg and update index to be in order.
+    df = pd.DataFrame(data).sort_values('phase')
+    df.index = range(len(df))
+
+    # Generate gap column in DataFrame.
+    phases = df['phase'].tolist()
+    gaps = [phases[i] - phases[i - 1] for i in range(len(phases))]
+    gaps[0] += 360
+    df['gap'] = gaps
 
     if adjust is True:
-        adjustedLatArgs = adjustLatitudeArguments(df['latArg'].tolist())
-        df = df.assign(latArg=adjustedLatArgs).sort_values('latArg', ascending=False)
+        df = adjustPhase(df)
+        # maxValue = df['phase'].max()
+        # df['phase'] = df['phase'].apply(lambda x: x - maxValue)
+        # df.index = range(len(df))
 
     return df
 
 
-def separateBatch(df: pd.DataFrame) -> list[pd.DataFrame]:
-    rtn = []
-    
+def splitBatch(df: pd.DataFrame, ignore=None) -> list[pd.DataFrame]:
+    # As an implementation detail, the last row is always the largest gap, so we don't need to compute max.
+    if ignore is None:
+        ignore = []
+    if -1 in ignore:
+        ignore[ignore.index(-1)] = len(df) - 1
+    outliers = findOutliersIQR(df.drop(ignore)['gap'])
+
+    # If there are no outliers besides the first gap, there is only one group.
+    if len(outliers) == 0:
+        return [df.copy()]
+
+    indices = list(outliers.index)
+    if len(df) - 1 not in indices:
+        indices.append(len(df))
+    groups = []
+    idx = 0
+    for i in indices:
+        g = df.iloc[idx:i+1].sort_values('phase', ascending=False)
+        groups.append(g.reset_index(drop=True))
+        idx = i+1
+
+    return groups
+
+
+def refineBatch(groups: list[pd.DataFrame]) -> list[pd.DataFrame]:
+    rtn = [g for g in groups]
+    count = -1
+    while len(rtn) != count:
+        count = len(rtn)
+        tmp = []
+        for i, group in enumerate(rtn):
+            if i == 0:
+                ignore = []
+            elif i == count - 1:
+                ignore = [0, -1]
+            else:
+                ignore = []
+
+            tmp.append(splitBatch(group, ignore=ignore))
+
+        rtn = [element for subArr in tmp for element in subArr]
+
+    return rtn
+
+
+def getDataFrame(group: str, launch: str, jd, adjust=True):
+    tleList = fetchTLEs(group, launch)
+    return generateDataFrame(tleList, jd, adjust=adjust)
+
+
+def getSplitFrames(group: str, launch: str, jd):
+    tleList = fetchTLEs(group, launch)
+    df = generateDataFrame(tleList, jd, adjust=True)
+    groups = splitBatch(df, [-1])
+    refinedGroups = refineBatch(groups)
+    return refinedGroups
 
 
 def main(group: str, launch: str):
@@ -118,10 +208,9 @@ def main(group: str, launch: str):
     if not _checkInput(launch):
         raise TypeError(f'launch must be an int or a str digit')
 
-    tleList = fetchTLEs(group, launch)
     jd = now()
-    df = buildDataFrame(tleList, jd)
-    print(df)
+    frames = getSplitFrames(group, launch, jd)
+    pp(frames)
 
     return 0
 
