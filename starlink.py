ccedbc83132a6581675e7393379390f82590bdac
qbarnes <quinton.barnes68@gmail.com>
2023-09-25 00:05:56 -0500

    added passes to sub-batches

--- starlink.py
+++ starlink.py
@@ -2,13 +2,26 @@ import sys
 import tomllib
 from math import degrees
 
+import geocoder
 import pandas as pd
+from sattrack.coordinates import GeoPosition
+from sattrack.exceptions import TLEException
 from sattrack.orbit import Satellite
 from sattrack.sgp4 import TwoLineElement
-from sattrack.spacetime import now
+from sattrack.spacetime import now, JulianDate
+from sattrack.topocentric import getNextPass
 from sattrack.util import EARTH_EQUITORIAL_RADIUS
 
 from sortgroups import importStarlinkTLE
+
+
+def jdRound(self, n):
+    digits = int(self.time().rsplit('.', 1)[1])
+    value = self.value - (digits / 86400)
+    return JulianDate.fromNumber(value, self.timezone)
+
+
+JulianDate.__round__ = jdRound
 
 
 def importConfig(filename):
@@ -38,7 +51,18 @@ def fetchTLEs(group: str, launch: str, toml=None):
     tleStringList = importStarlinkTLE('starlink.tle')
     tleMasterList = list((TwoLineElement(tle) for tle in tleStringList))
 
-    return list((tle for tle in tleMasterList if int(tle.line1[9:14]) == intDes))
+    rtn = list((tle for tle in tleMasterList if int(tle.line1[9:14]) == intDes))
+
+    if not rtn:
+        raise TLEException(f'no TLE data for batch {group}-{launch}')
+
+    return rtn
+
+
+def fetchTLEs2() -> dict[str, TwoLineElement]:
+    tleStringList = importStarlinkTLE('starlink.tle')
+
+    return {tle.split('\n', 1)[0].rstrip(): TwoLineElement(tle) for tle in tleStringList}
 
 
 def _checkInput(value):
@@ -223,7 +247,7 @@ def splitIntoPlanes(df: pd.DataFrame) -> list[pd.DataFrame]:
     return planes
 
 
-def splitIntoBatches(df: pd.DataFrame):
+def splitIntoBatches(df: pd.DataFrame) -> list[pd.DataFrame]:
     """Return a list of DataFrames where each DataFrame pertains to a batch of satellites dependent on phase angle. All
     satellites of df should be in a single orbital plane."""
 
@@ -233,7 +257,7 @@ def splitIntoBatches(df: pd.DataFrame):
     return refinedGroups
 
 
-def getLaunchData(group: str, launch: str, jd):
+def getLaunchData(group: str, launch: str, jd) -> list[list[pd.DataFrame]]:
     """Compute the DataFrames for each batch in each orbital plane."""
 
     tleList = fetchTLEs(group, launch)
@@ -242,6 +266,100 @@ def getLaunchData(group: str, launch: str, jd):
     refinedGroups = [splitIntoBatches(plane) for plane in planes]
 
     return refinedGroups
+
+
+def getBatchTimes(df: pd.DataFrame, geo: GeoPosition, jd: JulianDate) -> (JulianDate, JulianDate):
+    tleMap = fetchTLEs2()
+    tleList = [tleMap[f'STARLINK-{satId}'] for satId in df['sat-id']]
+    passList = [getNextPass(Satellite(tle), geo, jd) for tle in tleList]
+
+    # Find the next series of passes where at least one pass is visible.
+    anyVisible = any([p.visible for p in passList])
+    while not anyVisible:
+        passList = [getNextPass(Satellite(tle), geo, np) for tle, np in zip(tleList, passList)]
+        anyVisible = any([p.visible for p in passList])
+
+    # Find the first time any sat becomes visible, and the first time any sat disappears.
+    appearsIndex = -1
+    appears, disappears = None, None
+    for i, p in enumerate(passList):
+        if p.visible:
+            startTimes = [info for info in (p.riseInfo, p.firstUnobscuredInfo, p.firstIlluminatedInfo) if
+                          info is not None]
+            appears = min(startTimes, key=lambda o: o.time)
+            appearsIndex = i
+            break
+
+    # If passList is a single pass, we want to look at the same pass, so index should be -1, so it becomes 0 when we
+    # add 1.
+    if len(passList) == 1:
+        appearsIndex = -1
+    for p in reversed(passList[appearsIndex+1:]):
+        if p.visible:
+            endTimes = [info for info in (p.setInfo, p.lastUnobscuredInfo, p.lastIlluminatedInfo) if
+                        info is not None]
+            disappears = min(endTimes, key=lambda o: o.time)
+            break
+
+    maxInfos = []
+    for p in passList:
+        visibleInfos = [info for info in (p.maxInfo, p.lastIlluminatedInfo, p.lastUnobscuredInfo, p.setInfo)
+                        if info is not None and info.visible]
+        maxInfos.append(max(visibleInfos, key=lambda o: o.altitude))
+
+    # maxInfo = max([p.maxInfo for p in passList], key=lambda o: o.altitude)
+    maxInfo = max(maxInfos, key=lambda o: o.altitude)
+
+    return appears, maxInfo, disappears
+
+
+def addPassInfo(df: pd.DataFrame, geo, jd):
+    tleMap = fetchTLEs2()
+    tleList = [tleMap[f'STARLINK-{satId}'] for satId in df['sat-id']]
+    passList = [getNextPass(Satellite(tle), geo, jd) for tle in tleList]
+
+    # df['rises'] = [p.riseInfo.time.time() for p in passList]
+    # df['max-alt'] = [round(p.maxInfo.altitude, 2) for p in passList]
+    # df['sets'] = [p.setInfo.time.time() for p in passList]
+
+    df['atime'] = [p.riseInfo.time.time().rsplit('.', 1)[0] for p in passList]
+    df['aalt'] = [p.riseInfo.altitude for p in passList]
+    df['aaz'] = [p.riseInfo.azimuth for p in passList]
+    df['mtime'] = [p.maxInfo.time.time().rsplit('.', 1)[0] for p in passList]
+    df['malt'] = [p.maxInfo.altitude for p in passList]
+    df['maz'] = [p.maxInfo.azimuth for p in passList]
+    df['dtime'] = [p.setInfo.time.time().rsplit('.', 1)[0] for p in passList]
+    df['dalt'] = [p.setInfo.altitude for p in passList]
+    df['daz'] = [p.setInfo.azimuth for p in passList]
+
+
+def getPassInfo(df: pd.DataFrame, geo: GeoPosition, jd: JulianDate, visibleNumber=1):
+    tleMap = fetchTLEs2()
+    tleList = [tleMap[f'STARLINK-{satId}'] for satId in df['sat-id']]
+
+    passList = []
+    for tle in tleList:
+        sat = Satellite(tle)
+        currentNumber = 0
+
+        np = getNextPass(sat, geo, jd)
+        if np.visible:
+            currentNumber = 1
+
+        while currentNumber < visibleNumber:
+            # Some satellites wont produce visible passes, so stop after 7 days.
+            if np.riseInfo.time - jd > 7:
+                np = None
+                break
+
+            np = getNextPass(sat, geo, np)
+
+            if np.visible:
+                currentNumber += 1
+
+        passList.append(np)
+
+    return passList
 
 
 # Functions for ease of use if importing module.
@@ -263,22 +381,53 @@ def getSplitFrames(group: str, launch: str, jd):
     return refinedGroups
 
 
-def main(group: str, launch: str):
+def main(group: str, launch: str, geo: GeoPosition, jd: JulianDate, includePass=False):
     if not _checkInput(group):
         raise TypeError(f'group must be an int or a str digit')
     if not _checkInput(launch):
         raise TypeError(f'launch must be an int or a str digit')
 
-    jd = now()
-    data = getLaunchData(group, launch, jd)
+    try:
+        data = getLaunchData(group, launch, jd)
+    except TLEException as e:
+        print('error:', e.message)
+        return 1
+
     for i, plane in enumerate(data, 1):
-        print('Plane ', i)
+        print('Plane', i, 'of', len(data))
         for j, grp in enumerate(plane, 1):
-            print('Group ', j)
+            print('\nGroup', j, 'of', len(plane))
+            if includePass:
+                appears, maxInfo, disappears = getBatchTimes(grp, geo, jd)
+                print('    appears:\t', appears.time.date(), '\talt:', round(appears.altitude, 2), '\taz:',
+                      round(appears.azimuth, 2))
+                print('    max:\t', maxInfo.time.date(), '\talt:', round(maxInfo.altitude, 2), '\taz:',
+                      round(maxInfo.azimuth, 2))
+                print('    disappears:\t', disappears.time.date(), '\talt:', round(disappears.altitude, 2), '\taz:',
+                      round(disappears.azimuth, 2))
             print(grp.round(2))
+            # if includePass:
+            #     # addPassInfo(grp, geo, jd)
+            #     passes = getPassInfo(grp, geo, jd, 1)
+            #     for (_, row), np in zip(grp.round(2).iterrows(), passes):
+            #         print(row.to_frame().T.to_string(header=False))
+            #         riseString = f'\tappears: {np.riseInfo.time.date()} - alt: {round(np.riseInfo.altitude)} - az: ' \
+            #                      f'{round(np.riseInfo.azimuth)}'
+            #         maxString = f'\tappears: {np.maxInfo.time.date()} - alt: {round(np.maxInfo.altitude)} - az: ' \
+            #                     f'{round(np.maxInfo.azimuth)}'
+            #         setString = f'\tappears: {np.setInfo.time.date()} - alt: {round(np.setInfo.altitude)} - az: ' \
+            #                     f'{round(np.setInfo.azimuth)}'
+            #         print(riseString, maxString, setString)
+            # else:
+            #     print(grp.round(2))
 
     return 0
 
 
 if __name__ == '__main__':
-    exit(main(sys.argv[1], sys.argv[2]))
+    groupArg = sys.argv[1]
+    launchArg = sys.argv[2]
+    gc = geocoder.ip('me')
+    geoArg = GeoPosition(gc.latlng[0], gc.latlng[1])
+    jdArg = now()
+    exit(main(groupArg, launchArg, geoArg, jdArg, True))
